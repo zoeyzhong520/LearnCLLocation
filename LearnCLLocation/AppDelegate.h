d79d6deb924c2dbab87ee6be5759e57f44dab2da
AsiaZhang <17612156438@sina.cn>
2018-06-06 12:03:15 +0800

    init commit

---
+++ LearnCLLocation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LearnCLLocation
+//
+//  Created by zhifu360 on 2018/6/6.
+//  Copyright © 2018年 智富金融. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LearnCLLocation/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LearnCLLocation
+//
+//  Created by zhifu360 on 2018/6/6.
+//  Copyright © 2018年 智富金融. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
